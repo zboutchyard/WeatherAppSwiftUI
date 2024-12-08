3d0e8002b61a5949afc727c87d290abc606f2bf5
zboutchyard <zackboutchyard@gmail.com>
2024-12-08 09:49:34 -0500

    added comment

--- WeatherAppSwiftUI/WeatherFetching.swift
+++ WeatherAppSwiftUI/WeatherFetching.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 protocol WeatherFetching {
+    // made protocol to make testing easier through use of mocks. Could use playground dependency resolver in test to return different instance/ mock response of Weather.
     func fetchWeather(for city: String) async throws -> Weather
 }
